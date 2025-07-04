a59ec5ca0f7cedf57131431661278244f2d6da4b
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2025-07-04 05:19:27 -0300

    add utils

---
+++ src/services/constants.js
@@ -0,0 +1,16 @@
+export const memberFromOptions = [
+  { value: "0", label: "Primera vez" },
+  { value: "1", label: "Menos de 1 mes" },
+  { value: "2", label: "1 a 3 meses" },
+  { value: "3", label: "3 a 6 meses" },
+  { value: "4", label: "6 meses a 1 año" },
+  { value: "5", label: "1 a 2 años" },
+  { value: "6", label: "Más de 2 años" },
+];
+
+export const attendWithOptions = [
+  { value: "1", label: "Solo" },
+  { value: "2", label: "Pareja" },
+  { value: "3", label: "Niños" },
+  { value: "4", label: "Pareja y niños" }
+];
--- src/services/scripts.js
+++ src/services/scripts.js
@@ -2,6 +2,20 @@ const getDate = () => {
   return new Date().toISOString();
 }
 
+const getCommunity = (age) => {
+  switch(age){
+    case age < 25:
+      return 1
+    case age < 35:
+      return 2
+    case age >= 35:
+      return 3    
+    default:
+      return 2;
+  }
+};
+
 export {
-  getDate
+  getDate,
+  getCommunity
 }
